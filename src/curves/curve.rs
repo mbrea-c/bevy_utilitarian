11f530f8bb5440fbe03aa1b1988950705695e415
Manuel Brea Carreras <m.brea.carreras@gmail.com>
2024-02-19 21:17:18 +0000

    Fixed problem where alpha blending was not working (#3)

--- src/curves/curve.rs
+++ src/curves/curve.rs
@@ -11,7 +11,7 @@ pub trait AsParamCurve<P: Point> {
     fn get(&self, t: f32) -> P;
 }
 
-#[derive(Reflect, Clone, Serialize, Deserialize)]
+#[derive(Reflect, Clone, Serialize, Deserialize, Debug)]
 pub enum ParamCurve<P: Point> {
     Linear(LinearParamCurve<P>),
     Constant(ConstantParamCurve<P>),
@@ -41,3 +41,35 @@ impl<P: Point> AsParamCurve<P> for ParamCurve<P> {
 }
 
 pub type Gradient = ParamCurve<ColorPoint>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bevy::render::color::Color;
+
+    #[test]
+    fn gradient_alpha_blending_works() {
+        let grad = Gradient::linear_uniform(vec![
+            ColorPoint::rgba(1., 1., 1., 1.),
+            ColorPoint::rgba(1., 1., 1., 0.),
+        ]);
+
+        let mid = grad.get(0.5);
+
+        assert!(mid.a() - 0.5 < 0.000001);
+    }
+
+    #[test]
+    fn gradient_alpha_blending_works_large() {
+        let grad = Gradient::linear(vec![
+            (0., Color::rgba(300., 100., 1., 1.).into()),
+            (0.7, Color::rgba(3., 1., 1., 1.).into()),
+            (0.8, Color::rgba(1., 0.3, 0.3, 1.).into()),
+            (0.9, Color::rgba(0.3, 0.3, 0.3, 1.).into()),
+            (1., Color::rgba(0.1, 0.1, 0.1, 0.).into()),
+        ]);
+
+        let col = *grad.get(0.9343);
+        assert!(col.a() - (1. - 0.343) < 0.000001);
+    }
+}
