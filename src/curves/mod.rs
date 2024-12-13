a970e23170b0b00f5c9caaacb889813e556a3b26
Manuel Brea <m.brea.carreras@gmail.com>
2024-12-13 20:31:23 +0000

    Remove curves now that Bevy has builtin curve abstractions

--- src/curves/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod constant;
-pub mod curve;
-pub mod linear;
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,9 @@
-pub mod curves;
 pub mod geometric;
 pub mod randomized_values;
 pub mod steppers;
 
 pub mod prelude {
     use super::*;
-    pub use curves::{
-        constant::ConstantParamCurve,
-        curve::{AsParamCurve, Gradient, ParamCurve},
-        linear::LinearParamCurve,
-    };
     pub use geometric::{pitchyaw::PitchYaw, pitchyawclamped::PitchYawClamped};
     pub use randomized_values::{RandF32, RandValue, RandVec3};
     pub use steppers::{
